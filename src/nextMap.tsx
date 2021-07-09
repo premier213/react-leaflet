0c3afe833d8813887c528679181620d61043eb9b
premier213 <premierp1213@gmail.com>
2021-07-09 08:06:43 +0430

    feat:create type & props

--- src/nextMap.tsx
+++ src/nextMap.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './styles/index.css';
 
-export const NextMap = () => {
-  const position = { lat: 32.62524, lng: 51.66534 }
+interface EnteredProps {
+  scrollWheelZoom: boolean;
+  zoomControl: boolean;
+  zoom: number;
+  lat: number;
+  lng: number;
+  height: string;
+  width: string;
+  attributionControl: boolean;
+}
+
+export const NextMap = (props: EnteredProps) => {
+  const {
+    scrollWheelZoom,
+    zoomControl,
+    zoom,
+    lat,
+    lng,
+    height,
+    width,
+    attributionControl,
+  } = props;
+  const position = { lat: lat, lng: lng };
   React.useEffect(() => {
     const L = require('leaflet');
 
@@ -13,19 +34,28 @@ export const NextMap = () => {
       iconRetinaUrl:
         'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
       iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-      shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+      shadowUrl:
+        'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
     });
   }, []);
-    return (<MapContainer center={position} zoom={17} scrollWheelZoom={true} attributionControl={false} zoomControl={false} style={{ height: "300px", width: "300px" }}>
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>)
-}
-
+  return (
+    <MapContainer
+      center={position}
+      zoom={zoom}
+      scrollWheelZoom={scrollWheelZoom}
+      attributionControl={attributionControl}
+      zoomControl={zoomControl}
+      style={ { height: height , width: width }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
