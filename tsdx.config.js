93cfd23effc38e6896112797ca2ed451756b006c
premier213 <premierp1213@gmail.com>
2021-07-09 08:09:42 +0430

    fix:package.json

--- tsdx.config.js
+++ tsdx.config.js
@@ -3,16 +3,6 @@ const images = require('rollup-plugin-image-files');
 
 module.exports = {
   rollup(config, options) {
-    config.plugins = [
-      images({ incude: ['**/*.png', '**/*.jpg'] }),
-      ...config.plugins,
-    ];
-
-    return config;
-  },
-};
-module.exports = {
-  rollup(config, options) {
     config.plugins.push(
         postcss({
         plugins: [],
